f8ef0dedf055dc3647d9404ecf0a8cfa8d4eec48
guhyeonseo <guhamsi@naver.com>
2026-07-31 14:36:04 +0900

    1
    1

--- commission_frontend/src/features/payment/components/PaymentButton.jsx
+++ commission_frontend/src/features/payment/components/PaymentButton.jsx
@@ -3,49 +3,32 @@ import { createPayment } from "../api/paymentApi";
 
 import "./PaymentButton.css";
 
-export default function PaymentButton({
-  commissionId
-}) {
-
+export default function PaymentButton({ commissionId }) {
   const handlePayment = async () => {
-
     try {
-
-      const data = await createPayment(
-        commissionId
-      );
+      const data = await createPayment(commissionId);
 
       const { orderId, amount } = data;
 
-      const tossPayments =
-        await loadTossPayments(
-          "test_ck_oEjb0gm23PLJgdZDxL063pGwBJn5"
-        );
-
-      await tossPayments.requestPayment(
-        "카드",
-        {
-          amount,
-          orderId,
-          orderName: "커미션 결제",
-          customerName: "테스트유저",
-          successUrl: "http://localhost:5173/payment/success",
-          failUrl: "http://localhost:5173/payment/fail"
-        }
+      const tossPayments = await loadTossPayments(
+        "test_ck_oEjb0gm23PLJgdZDxL063pGwBJn5",
       );
 
+      await tossPayments.requestPayment("카드", {
+        amount,
+        orderId,
+        orderName: "커미션 결제",
+        customerName: "테스트유저",
+        successUrl: "https://commission-site-ghs1.vercel.app/payment/success",
+        failUrl: "https://commission-site-ghs1.vercel.app/payment/fail",
+      });
     } catch (error) {
-
       console.error(error);
-
     }
   };
 
   return (
-    <button
-      className="payment-btn"
-      onClick={handlePayment}
-    >
+    <button className="payment-btn" onClick={handlePayment}>
       결제하기
     </button>
   );
